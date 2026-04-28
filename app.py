723288bb4b51a2bb5fbbe8a85f6bd256810e8507
CarolineAnne <157393678+CarolineAnne@users.noreply.github.com>
2026-04-28 12:26:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1145,8 +1145,11 @@ if st.session_state.logado:
     
         # ================= ADMIN: ANEXAR PDF E CONCLUIR =================
         if nivel == "admin":
+    
+            st.divider()
+            st.subheader("👑 Ações do Administrador")
 
-            # 🔎 TESTE TEMPORÁRIO DO BUCKET
+            # 🔎 TESTE TEMPORÁRIO DO STORAGE
             st.write("Projeto Supabase usado:", url)
             st.write("Bucket esperado:", "arquivos")
         
@@ -1155,10 +1158,10 @@ if st.session_state.logado:
                 st.write("Buckets encontrados:", buckets)
             except Exception as e:
                 st.error(f"Erro ao listar buckets: {e}")
-
-    
-            st.divider()
-            st.subheader("👑 Ações do Administrador")
+        
+            st.stop()  # 🔥 para aqui só para testar
+        
+            arquivo_pdf = st.file_uploader(
     
             arquivo_pdf = st.file_uploader(
                 "Anexar PDF",
